05c789300002ecdfb2c72efd2460cb15523f249a
exploitx632 <exploitx632hql@gmail.com>
2025-11-05 16:44:34 +0800

    js numbers

---
+++ module-1/script.js
@@ -0,0 +1,21 @@
+// // variable
+// var name = 'shaon';
+// let age = 23;
+// const nationality = 'Bangladeshi';
+
+// let age = 23;
+// console.log(age);
+
+// const age = "23";
+// const parseAge = parseInt(age);
+// console.log(age,parseAge)
+
+// const num1 = 0.1;
+// const num2 = 0.2;
+// console.log((num1+num2).toFixed(2))
+
+
+const num = 43;
+const num2 = -32;
+console.log(num / 0);
+console.log(num2/ 0);
