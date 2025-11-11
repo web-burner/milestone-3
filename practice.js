5675697156a19b5d65b416e988046d7983c940b5
exploitx632 <exploitx632hql@gmail.com>
2025-11-11 17:01:53 +0800

    added practice

---
+++ practice.js
@@ -0,0 +1,47 @@
+// // 1.	দুটি integer সংখ্যা যোগ করো
+// const int1 = 19;
+// const int2 = 23;
+// console.log(int1 + int2);
+
+// // 2.	একটি float এবং integer সংখ্যা দিয়ে ভাগ করো
+
+// const float1 = 20.5;
+// const int = 5;
+// console.log(float1 / int);
+
+// // 3.	শতকরা হিসাব বের করো (যেমন 45 এর 20%)
+
+// const num = 45;
+// const percentage = 20/100;
+// console.log(num * percentage);
+
+// // 4.	৩টি সংখ্যা যোগ করে average বের করো
+
+// const num1 = 5;
+// const num2 = 10;
+// const num3 = 20;
+// const sum = num1 + num2 + num3;
+// const average = sum / 3;
+// console.log(average)
+
+// 5.	৩টি সংখ্যার মধ্যে max ও min খুঁজে বের করো
+
+
+// 3. Variable Practice
+
+// Task:
+// // 	1.	var, let, const দিয়ে আলাদা ৩টি variable ঘোষণা করো
+// var name = 'shaon';
+// let userName = 'shaon';
+// const UserName = 'shaon';
+// // 	2.	var দিয়ে তৈরি করা variable আবার re-declare করে নতুন মান দাও
+// var name = 'Sajjat'
+// // 	3.	let দিয়ে তৈরি করা variable এর মান আপডেট করো
+// userName = 'sajjat';
+// // 	4.	const দিয়ে তৈরি করা variable আপডেট করার চেষ্টা করো (কি error আসে দেখো)
+// UserName = 'S'
+// // 	5.	কোনও variable ঘোষণা করো কিন্তু মান দিও না — তারপর console.log দিয়ে চেক করো
+// let noValue ;
+// console.log(noValue)
+
+
